60f9d13712d6c39303a8a55a9e0393207865a0ef
Xavier Meroño <x.merono@gmail.com>
2017-03-09 15:52:04 +0100

    express added

---
+++ app.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const path = require('path')
+const app = express()
+
+const PORT = process.env.PORT || 3000
+const publicFolder = path.join(__dirname, 'public')
+
+app
+  .use(express.static(publicFolder))
+  .listen(PORT, () => console.log(`Magic happens on Port ${PORT}...`))
