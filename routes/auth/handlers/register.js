9b1a7fdab53a197c042af467c26c9d416bd58f6a
Xavier Meroño <x.merono@gmail.com>
2017-03-16 16:42:01 +0100

    yolocation fixed

--- routes/auth/handlers/register.js
+++ routes/auth/handlers/register.js
@@ -2,12 +2,12 @@ const User = require('../../../models/User')
 
 function register(req, res) {
 
-  const { username, password, email } = req.body
+  const { username, password, email, location } = req.body
   const account = new User({ username, email, location })
 
   User.register( account, password, err => {
     if (err) {
-      return res.json({success: false, msg: `Username already exists.${err}`})
+      return res.json({success: false, msg: 'Username already exists.' })
     }
     res.json({success: true, msg: 'Successful created new user.'})
   })
